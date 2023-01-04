b4bebdc79eceb3b290dcf9c6ccae84e12b0a6844
KKaiyrbekov <han_umag@mail.com>
2023-01-04 18:42:43 +0600

    added system properties test

--- src/test/java/tests/properties/SystemPropertiesTests.java
+++ src/test/java/tests/properties/SystemPropertiesTests.java
@@ -26,11 +26,37 @@ public class SystemPropertiesTests {
     }
 
     @Test
-    @Tag("one_property_test")
     void simplePropertyThreeTest() {
         System.setProperty("browser", "opera");
         String browserName = System.getProperty("browser", "firefox");
         System.out.println(browserName); // opera
+    }
+
+    @Test
+    @Tag("one_property_test")
+    void simplePropertyFourTest() {
+        String browserName = System.getProperty("browser", "firefox");
+        System.out.println(browserName);
+        // gradle clean one_property -Dbrowser=safari
+        // safari
+    }
+
+    @Test
+    @Tag("many_properties_test")
+    void simplePropertyFiveTest() {
+        String browserName = System.getProperty("browser", "firefox");
+        String browserVersion = System.getProperty("browser_version", "105");
+        String browserSize = System.getProperty("browser_size", "1920x1080");
+
+        System.out.println(browserName);
+        System.out.println(browserVersion);
+        System.out.println(browserSize);
+    }
+
+    @Test
+    @Tag("hello")
+    void simplePropertySixTest() {
+        System.out.println("Hello, " + System.getProperty("user_name", "unknown student"));
     }
 }
 
